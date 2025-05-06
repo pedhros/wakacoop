d480ba26d26625aa3e33061f0dbb9da511496ce9
Pedro Santos <pedrooohsss@gmail.com>
2025-05-05 21:31:43 -0300

    inicia projeto

---
+++ src/main/java/academy/wakanda/wakacoop/WakacoopApplication.java
@@ -0,0 +1,13 @@
+package academy.wakanda.wakacoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakacoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WakacoopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/academy/wakanda/wakacoop/WakacoopApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.wakanda.wakacoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakacoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
